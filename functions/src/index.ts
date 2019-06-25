41fd663a75010e7499d92f8ea4cb8c72ca9bd245
Maximilian Burkhardt <squid219@gmail.com>
2019-06-25 00:05:51 -0700

    Progress on Space Track loading, function now logs in and downloads data

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,14 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
-import request from "request-promise";
-import { Response } from "request";
+import request, {
+  RequestPromise,
+  RequestPromiseOptions
+} from "request-promise";
+import { RequestAPI, RequiredUriUrl } from "request";
 
-const app = admin.initializeApp();
+const app = admin.initializeApp({
+  storageBucket: functions.config().storage.bucket
+});
 
 export type SatelliteTle = {
   name: string;
@@ -11,9 +16,11 @@ export type SatelliteTle = {
   line2: string;
 };
 
-function spaceTrackLogin() {
+async function spaceTrackLogin(
+  requestApi: RequestAPI<RequestPromise, RequestPromiseOptions, RequiredUriUrl>
+) {
   const LOGIN_URL = "https://www.space-track.org/ajaxauth/login";
-  return request({
+  const response = await requestApi({
     uri: LOGIN_URL,
     method: "POST",
     formData: {
@@ -22,43 +29,49 @@ function spaceTrackLogin() {
     },
     resolveWithFullResponse: true
   });
+  console.log(
+    `Space-Track login status: ${response.statusCode} ${response.statusMessage}`
+  );
+  return response;
+}
+
+async function downloadTle(
+  requestApi: RequestAPI<RequestPromise, RequestPromiseOptions, RequiredUriUrl>
+) {
+  const SPACETRACK_AMATEUR_CURATED_URL =
+    "https://www.space-track.org/basicspacedata/query/class/tle_latest/ORDINAL/1/EPOCH/%3Enow-30/orderby/NORAD_CAT_ID/format/3le/favorites/Amateur";
+  const response = await requestApi({
+    uri: SPACETRACK_AMATEUR_CURATED_URL,
+    resolveWithFullResponse: true
+  });
+  console.log(
+    `TLE fetch status: ${response.statusCode} ${response.statusMessage}`
+  );
+  const data: string[] = response.body
+    .replace(/\r/g, "")
+    .split("\n")
+    .filter((line: string) => !(line.trim() === ""));
+  const parsed: Array<SatelliteTle> = [];
+  for (let i = 0; i < data.length; i += 3) {
+    parsed.push({
+      name: data[i],
+      line1: data[i + 1],
+      line2: data[i + 2]
+    });
+  }
+  return parsed;
 }
 
 export const refreshTle = functions.https.onRequest(
-  (incomingRequest, outgoingResponse) => {
-    const SPACETRACK_AMATEUR_CURATED_URL =
-      "https://www.space-track.org/basicspacedata/query/class/tle_latest/ORDINAL/1/EPOCH/%3Enow-30/orderby/NORAD_CAT_ID/format/3le/favorites/Amateur";
-    const parsed: Array<SatelliteTle> = [];
+  async (_incomingRequest, outgoingResponse) => {
     // Placeholder until we write to cloud storage
     let appName = app.name;
     appName = appName;
     // End placeholder
-    spaceTrackLogin()
-      .then(function(loginResponse: Response) {
-        console.log(
-          `LOGIN STATUS: ${loginResponse.statusCode}, ${loginResponse.statusMessage}`
-        );
-        console.log(`HEADERS: ${JSON.stringify(loginResponse.headers)}`);
-        return "placeholder";
-      })
-      .then(function() {
-        request({
-          uri: SPACETRACK_AMATEUR_CURATED_URL,
-          resolveWithFullResponse: true
-        }).then(function(response: Response) {
-          console.log(
-            `RESPONSE STATUS: ${response.statusCode} ${response.statusMessage} BODY: ${response.body}`
-          );
-          let data: string[] = response.body.split("\n");
-          for (let i = 0; i < data.length; i += 3) {
-            parsed.push({
-              name: data[i],
-              line1: data[i + 1],
-              line2: data[i + 2]
-            });
-          }
-          outgoingResponse.send(JSON.stringify(parsed));
-        });
-      });
+    const cookieJar = request.jar();
+    const customRequest = request.defaults({ jar: cookieJar });
+    await spaceTrackLogin(customRequest);
+    const parsed = await downloadTle(customRequest);
+    return outgoingResponse.send(JSON.stringify(parsed));
   }
 );
