0e5d57c89158795a91adeebb72bdeb9c2ff3476d
Maximilian Burkhardt <squid219@gmail.com>
2019-07-15 22:38:18 -0700

    Prettier

--- src/components/Radar.tsx
+++ src/components/Radar.tsx
@@ -10,7 +10,7 @@ export type Props = {
 export type State = {
   width: number;
   height: number;
-}
+};
 
 class Radar extends React.Component<Props, State> {
   constructor(props: Props) {
@@ -28,14 +28,26 @@ class Radar extends React.Component<Props, State> {
     this.setState({ width: width, height: height });
   }
 
-  generateSatIcon(sat: SatellitePosition, xOffset: number, yOffset: number, radius: number): JSX.Element {
+  generateSatIcon(
+    sat: SatellitePosition,
+    xOffset: number,
+    yOffset: number,
+    radius: number
+  ): JSX.Element {
     const xPos = radius * Math.cos(sat.azimuth - Math.PI / 2) + xOffset;
     const yPos = radius * Math.sin(sat.azimuth - Math.PI / 2) + yOffset;
     const iconSize = 10;
     return (
       <React.Fragment>
-        <rect x={xPos - (iconSize / 2)} y={yPos - (iconSize / 2)} height={iconSize} width={iconSize} />
-        <text x={xPos + iconSize} y={yPos + 5}>{sat.name}</text>
+        <rect
+          x={xPos - iconSize / 2}
+          y={yPos - iconSize / 2}
+          height={iconSize}
+          width={iconSize}
+        />
+        <text x={xPos + iconSize} y={yPos + 5}>
+          {sat.name}
+        </text>
       </React.Fragment>
     );
   }
@@ -51,19 +63,49 @@ class Radar extends React.Component<Props, State> {
   }
 
   render() {
-    const radius = (Math.min(this.state.width, this.state.height) / 2) - 10;
+    const radius = Math.min(this.state.width, this.state.height) / 2 - 10;
     const xCenter = this.state.width / 2;
     const yCenter = this.state.height / 2;
     const uiColor = "rgb(0,0,0)";
-    const satellites = this.getOverhead().map((sat) => this.generateSatIcon(sat, xCenter, yCenter, radius));
+    const satellites = this.getOverhead().map(sat =>
+      this.generateSatIcon(sat, xCenter, yCenter, radius)
+    );
     return (
       <div className="radarDiv">
-        <ReactResizeDetector handleHeight handleWidth onResize={this.onResize} />
+        <ReactResizeDetector
+          handleHeight
+          handleWidth
+          onResize={this.onResize}
+        />
         <svg className="radarSvg" xmlns="http://www.w3.org/2000/svg">
-          <circle cx={xCenter} cy={yCenter} r={radius} fill="none" stroke={uiColor} />
-          <circle cx={xCenter} cy={yCenter} r={radius / 2} fill="none" stroke={uiColor} />
-          <line x1={0} y1={yCenter} x2={this.state.width} y2={yCenter} stroke={uiColor} />
-          <line x1={xCenter} y1={0} x2={xCenter} y2={this.state.height} stroke={uiColor} />
+          <circle
+            cx={xCenter}
+            cy={yCenter}
+            r={radius}
+            fill="none"
+            stroke={uiColor}
+          />
+          <circle
+            cx={xCenter}
+            cy={yCenter}
+            r={radius / 2}
+            fill="none"
+            stroke={uiColor}
+          />
+          <line
+            x1={0}
+            y1={yCenter}
+            x2={this.state.width}
+            y2={yCenter}
+            stroke={uiColor}
+          />
+          <line
+            x1={xCenter}
+            y1={0}
+            x2={xCenter}
+            y2={this.state.height}
+            stroke={uiColor}
+          />
           {satellites}
         </svg>
       </div>
--- src/util/SharedTypes.ts
+++ src/util/SharedTypes.ts
@@ -1,4 +1,4 @@
-import {Moment} from "moment";
+import { Moment } from "moment";
 
 export type LatLong = {
   latitude: number;
