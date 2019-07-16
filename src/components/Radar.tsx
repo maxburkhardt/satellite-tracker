60174a28732bac7c3484cf23ab7232b2505511e4
Maximilian Burkhardt <squid219@gmail.com>
2019-07-15 22:37:54 -0700

    Switch to SVG rendering for Radar

--- src/components/Radar.tsx
+++ src/components/Radar.tsx
@@ -1,89 +1,46 @@
 import React from "react";
 import { SatellitePosition, LatLong } from "../util/SharedTypes";
+import ReactResizeDetector from "react-resize-detector";
 
 export type Props = {
   userLocation: LatLong;
   satData: Array<SatellitePosition>;
 };
 
-class Radar extends React.Component<Props> {
-  private radarUi: React.RefObject<HTMLCanvasElement>;
-  private satellitesOnRadar: React.RefObject<HTMLCanvasElement>;
+export type State = {
+  width: number;
+  height: number;
+}
+
+class Radar extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
-    this.drawRadarStructure = this.drawRadarStructure.bind(this);
-    this.drawSatOnRadar = this.drawSatOnRadar.bind(this);
-    this.clearRadar = this.clearRadar.bind(this);
+    // Initial values that will immediately get overwritten by the size detector
+    this.state = {
+      width: 0,
+      height: 0
+    };
+    this.onResize = this.onResize.bind(this);
     this.getOverhead = this.getOverhead.bind(this);
-    this.radarUi = React.createRef();
-    this.satellitesOnRadar = React.createRef();
   }
 
-  // we assume the radar canvas is square
-  // the size is half the width along one side
-  drawRadarStructure() {
-    if (this.radarUi.current) {
-      const ctx = this.radarUi.current.getContext("2d");
-      const size = this.radarUi.current.width / 2;
-
-      if (ctx) {
-        // outer circle
-        ctx.beginPath();
-        ctx.ellipse(size, size, size, size, 0, 0, 2 * Math.PI);
-        ctx.stroke();
-
-        // inner circle
-        ctx.beginPath();
-        ctx.ellipse(size, size, size / 2, size / 2, 0, 0, 2 * Math.PI);
-        ctx.stroke();
-
-        // cross-hatch
-        ctx.beginPath();
-        ctx.moveTo(2, size);
-        ctx.lineTo(size * 2, size);
-        ctx.stroke();
-        ctx.beginPath();
-        ctx.moveTo(size, 0);
-        ctx.lineTo(size, size * 2);
-        ctx.stroke();
-      }
-    }
+  onResize(width: number, height: number): void {
+    this.setState({ width: width, height: height });
   }
 
-  drawSatOnRadar(sat: SatellitePosition) {
-    if (this.satellitesOnRadar.current) {
-      const radarSize = this.satellitesOnRadar.current.width / 2;
-      const ctx = this.satellitesOnRadar.current.getContext("2d");
-      if (ctx) {
-        // r comes from the elevation of the satellite
-        // elevation 0 is at the far ring, elevation 1 is right in the center
-        const r = (1 - sat.elevation) * radarSize;
-        // do polar conversion, and account for the fact that a canvas origin
-        // in the top left
-        const x = r * Math.cos(sat.azimuth - Math.PI / 2) + radarSize;
-        const y = r * Math.sin(sat.azimuth - Math.PI / 2) + radarSize;
-
-        ctx.fillRect(x, y, 6, 6);
-        ctx.fillText(sat.name, x + 8, y);
-      }
-    }
+  generateSatIcon(sat: SatellitePosition, xOffset: number, yOffset: number, radius: number): JSX.Element {
+    const xPos = radius * Math.cos(sat.azimuth - Math.PI / 2) + xOffset;
+    const yPos = radius * Math.sin(sat.azimuth - Math.PI / 2) + yOffset;
+    const iconSize = 10;
+    return (
+      <React.Fragment>
+        <rect x={xPos - (iconSize / 2)} y={yPos - (iconSize / 2)} height={iconSize} width={iconSize} />
+        <text x={xPos + iconSize} y={yPos + 5}>{sat.name}</text>
+      </React.Fragment>
+    );
   }
 
-  clearRadar() {
-    if (this.satellitesOnRadar.current) {
-      const ctx = this.satellitesOnRadar.current.getContext("2d");
-      if (ctx) {
-        ctx.clearRect(
-          0,
-          0,
-          this.satellitesOnRadar.current.width,
-          this.satellitesOnRadar.current.height
-        );
-      }
-    }
-  }
-
-  getOverhead() {
+  getOverhead(): Array<SatellitePosition> {
     const overhead = [];
     for (const sat of this.props.satData) {
       if (sat.elevation > 0) {
@@ -93,31 +50,22 @@ class Radar extends React.Component<Props> {
     return overhead;
   }
 
-  componentDidMount() {
-    this.drawRadarStructure();
-    this.getOverhead().map(sat => this.drawSatOnRadar(sat));
-  }
-
-  componentDidUpdate() {
-    this.clearRadar();
-    this.getOverhead().map(sat => this.drawSatOnRadar(sat));
-  }
-
   render() {
+    const radius = (Math.min(this.state.width, this.state.height) / 2) - 10;
+    const xCenter = this.state.width / 2;
+    const yCenter = this.state.height / 2;
+    const uiColor = "rgb(0,0,0)";
+    const satellites = this.getOverhead().map((sat) => this.generateSatIcon(sat, xCenter, yCenter, radius));
     return (
       <div className="radarDiv">
-        <canvas
-          className="radarCanvas"
-          ref={this.radarUi}
-          width={300}
-          height={300}
-        />
-        <canvas
-          className="satellitesCanvas"
-          ref={this.satellitesOnRadar}
-          width={300}
-          height={300}
-        />
+        <ReactResizeDetector handleHeight handleWidth onResize={this.onResize} />
+        <svg className="radarSvg" xmlns="http://www.w3.org/2000/svg">
+          <circle cx={xCenter} cy={yCenter} r={radius} fill="none" stroke={uiColor} />
+          <circle cx={xCenter} cy={yCenter} r={radius / 2} fill="none" stroke={uiColor} />
+          <line x1={0} y1={yCenter} x2={this.state.width} y2={yCenter} stroke={uiColor} />
+          <line x1={xCenter} y1={0} x2={xCenter} y2={this.state.height} stroke={uiColor} />
+          {satellites}
+        </svg>
       </div>
     );
   }
