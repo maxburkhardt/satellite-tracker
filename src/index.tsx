c3ff7cea3530cafdeeca2cbb2411a84ce3add687
Maximilian Burkhardt <squid219@gmail.com>
2019-08-12 20:12:23 -0700

    Enable default service worker

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,4 @@ import TrackerContainer from './containers/TrackerContainer';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<TrackerContainer/>, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
